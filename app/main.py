374126d2224266415d6c620f7ce4824b54a62108
guff192 <kochkons1@ya.ru>
2022-05-04 23:22:21 +0400

    Named properly class and function

--- app/main.py
+++ app/main.py
@@ -5,26 +5,26 @@ from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from sqlalchemy import select
 
-import models
+from models import Base, Question
 from database import session_local, engine
 
-models.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
 # Dependency
-def get_db():
-    db = session_local()
+def get_session():
+    session = session_local()
     try:
-        yield db
+        yield session
     finally:
-        db.close()
+        session.close()
 
 
 # Pydantic Schema
 # Used as a serializer to output data from the api request
-class QuestionBase(BaseModel):
+class QuestionResponse(BaseModel):
     id: int
     question_text: str
     answer_text: str
@@ -34,11 +34,12 @@ class QuestionBase(BaseModel):
         orm_mode = True
 
 
-@app.get("/test/", response_model=QuestionBase)
-def get_question(db: Session = Depends(get_db)):
+@app.get("/test/", response_model=QuestionResponse)
+def get_question(db: Session = Depends(get_session)):
     # Assuming that we have some question in db
-    # q = models.Question(id=1, question_text="what?", answer_text="42", created_at=datetime.now())
+    # q = Question(question_text="what?", answer_text="42", created_at=datetime.now())
     # db.add(q)
     # db.commit()
 
-    return db.scalars(select(models.Question)).first()
+    return db.scalars(select(Question)).first()
+
