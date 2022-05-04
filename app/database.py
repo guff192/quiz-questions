6f302f22c698c5afdffb88c6db55ab6575d9d13e
guff192 <kochkons1@ya.ru>
2022-05-04 22:16:50 +0400

    Configured db engine to use echo mode

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:qwerty@localhost:5432/postgres"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
