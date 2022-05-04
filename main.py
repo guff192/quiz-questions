6f7374dc59b5ec3e56c46d0a6998e8f2a447c389
Sergey Gambaryan <sgamb2000@gmail.com>
2022-05-04 18:28:55 +0300

    Add sql model

---
+++ main.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+class Question(Base):
+    __tablename__ = "questions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    question = Column(String)
+    answer = Column(String)
+    date = Column(Date)
